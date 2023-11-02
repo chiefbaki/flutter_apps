f139b67a7dae7cc796a4fdeb3d1fc56677346e91
Islam <ik509331@gmail.com>
2023-11-02 13:28:00 +0300

    updated main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MainApp());
 
-
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
@@ -12,7 +11,10 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("First Screen of My app!", style: TextStyle(color: Colors.black),),
+          title: const Text(
+            "First Screen of My app!",
+            style: TextStyle(color: Colors.black),
+          ),
           centerTitle: true,
           backgroundColor: const Color.fromARGB(255, 7, 147, 255),
         ),
@@ -21,131 +23,59 @@ class MainApp extends StatelessWidget {
           child: const Row(
             children: [
               Padding(
-                padding: const EdgeInsets.only(left: 20, top: 20),
-                child: FirstBox(),
+                padding: EdgeInsets.only(left: 20, top: 20),
+                child: Box(position: MainAxisAlignment.start),
               ),
-              Padding(padding:EdgeInsets.only(left: 10, bottom: 50), 
-                child: SecondBox(),
+              Padding(
+                padding: EdgeInsets.only(left: 10, bottom: 50),
+                child: Box(position: MainAxisAlignment.center),
               ),
               Padding(
                 padding: EdgeInsets.only(left: 10, bottom: 65),
-                child: ThirdLess(),
+                child: Box(position: MainAxisAlignment.end),
               ),
             ],
           ),
         ),
       ),
-    ); 
-  }
-} 
-
-
-class FirstBox extends StatelessWidget {
-  const FirstBox({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                
-                Container(
-                  alignment: Alignment.center,
-                  child: Text("1"),
-                  width: 80,
-                  height: 80,
-                  color: Colors.red,
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: Text("2"),
-                  width: 95,
-                  height: 95,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: Text("3"),
-                  width: 115,
-                  height: 115,
-                  color: Colors.green,
-                ),
-              ],
-      ),
     );
   }
 }
 
 
-class SecondBox extends StatelessWidget {
-  const SecondBox({super.key});
+class Box extends StatelessWidget {
+  final MainAxisAlignment position;
+
+  const Box({super.key, required this.position});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  child: Text("1"),
-                  width: 80,
-                  height: 80,
-                  color: Colors.red,
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: Text("2"),
-                  width: 95,
-                  height: 95,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: Text("3"),
-                  width: 115,
-                  height: 115,
-                  color: Colors.green,
-                ),
-              ],
-      ),
-    );
-  }
-}
-
-
-class ThirdLess extends StatelessWidget {
-  const ThirdLess({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  child: Text("1"),
-                  width: 80,
-                  height: 80,
-                  color: Colors.red,
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: Text("2"),
-                  width: 95,
-                  height: 95,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: Text("3"),
-                  width: 115,
-                  height: 115,
-                  color: Colors.green,
-                ),
-              ],
+        mainAxisAlignment: position,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: Text("1"),
+            width: 80,
+            height: 80,
+            color: Colors.red,
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Text("2"),
+            width: 95,
+            height: 95,
+            color: Colors.yellow,
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Text("3"),
+            width: 115,
+            height: 115,
+            color: Colors.green,
+          ),
+        ],
       ),
     );
   }
